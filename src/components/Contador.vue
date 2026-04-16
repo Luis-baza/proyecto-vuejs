ae037606e68a49d21ffd9aefb2c0f6257c7990ce
luis-david <ludabazu@gmail.com>
2026-04-15 19:49:05 -0500

    mi primer componente

---
+++ src/components/Contador.vue
@@ -0,0 +1,28 @@
+
+<template>
+  <p>{{ contador }}</p>
+  
+  <button @click="incrementar">Incrementar</button>
+  <button @click="disminuir">Disminuir</button>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const contador = ref(0);
+
+const incrementar = () =>{
+  contador.value++;
+}
+const disminuir = () =>{
+  contador.value--;
+}
+
+
+
+</script>
+
+
+<style scoped>
+p{ color: red;}
+</style>
